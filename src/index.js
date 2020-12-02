c228e448ccd4ea72fca16dea36fb9b198431336e
cgyee <cyee2017@fau.edu>
2020-12-02 00:07:48 -0500

    added code comments to index.js & helper.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import {navbar} from './modules/navbar';
 import {menu} from './modules/menu';
 import {drink} from './modules/drink';
 
+
+//the key in tab options are associtated with their respective ids in the document 
 const tabOptions = {
     "Home": home(),
     "Menu": menu(),
     "Drink": drink()
 };
 
+//renders the home page on start adds navbar and home, other pages only pass one arguement
 renderPage(navbar(), home());
+
+//iterate over the tabs and where their tab id corresponds in tabOptions call the respective function on click
 const tabs = document.querySelectorAll('.tab'); 
 Array.from(tabs).forEach(tab => {
     tab.addEventListener('click',  e => {renderPage(tabOptions[tab.id])});
